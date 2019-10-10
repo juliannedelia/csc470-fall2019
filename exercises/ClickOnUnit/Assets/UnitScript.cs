af4e9b699675db368a5f0eb53fda49157fa0e87c
Julianne Delia <jd8928a@student.american.edu>
2019-10-10 16:38:07 -0400

    GoL updated

---
+++ exercises/ClickOnUnit/Assets/UnitScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitScript : MonoBehaviour
+{
+
+    public bool selected = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseOver()
+    {
+
+    }
+
+    private void OnMouseExit()
+    {
+        
+    }
+
+    private void OnMouseSelected()
+    {
+        selected = !selected;
+    }
+}
