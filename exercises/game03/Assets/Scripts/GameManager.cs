e1755f42322ae7982b530e67d79110a98e3e0e67
Julianne Delia <jd8928a@student.american.edu>
2019-10-28 15:59:46 -0400

    updated game03

---
+++ exercises/game03/Assets/Scripts/GameManager.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    UnitScript selectedUnit;
+
+    public GameObject talkBox;
+	public Text talkText;
+	public ToggleGroup actionSelectToggleGroup;
+	public GameObject selectedPanel;
+	public Text nameText;
+	public Image portraitImage;
+
+    public Image meterFG;
+    public GameObject healthMeterObject;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //SceneManager.LoadScene("title");
+    }
+
+    // Update is called once per frame
+    void Update()
+	{
+		if (Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject()) 
+        {
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+			
+			if (Physics.Raycast(ray, out hit, 9999)) 
+            {
+				if (hit.collider.gameObject.layer == LayerMask.NameToLayer("ground")) 
+                {
+					if (selectedUnit != null) 
+                    {
+						selectedUnit.destination = hit.point;
+					}
+				}
+			} 
+            else 
+            {
+				if (selectedUnit != null) 
+                {
+					selectedUnit.selected = false;
+					selectedUnit.setColorOnMouseState();
+					selectedUnit = null;
+
+					updateSelectedPanelUI();
+				}
+			}
+		}
+	}
+
+	public void selectUnit(UnitScript unit)
+	{
+		if(selectedUnit != null)
+		{
+			selectedUnit.selected = false;
+			selectedUnit.setColorOnMouseState();
+		}
+
+		selectedUnit = unit;
+
+		if(selectedUnit != null)
+		{
+			selectedUnit.selected = true;
+			selectedUnit.setColorOnMouseState();
+		}
+
+		updateSelectedPanelUI();
+	}
+
+	void updateSelectedPanelUI()
+	{
+		if(selectedUnit != null)
+		{
+			nameText.Text = selectedUnit.name;
+			portraitImage.sprite = selectedUnit.portrait;
+			selectedPanel.SetActive(true);
+		}
+		else
+		{
+			selectedPanel.SetActive(false);
+		}
+	}
+
+	public void TakeAction()
+	{
+		Vector3 pos = selectedUnit.transform.position + Vector3.up * 2;
+		pos = Camera.main.WorldToScreenPoint(pos);
+		talkBox.transform.position = pos;
+
+		IEnumerable<Toggle> activeToggles = actionSelectToggleGroup.ActiveToggles();
+		string action = "";
+		foreach(Toggle t in activeToggles)
+		{
+			if(t.IsActive())
+			{
+				action = t.gameObject.GetComponentInChildren<Text>().text;
+			}
+		}
+
+		StartCoroutine(displayTalkBoxMessages(new string[] {action, "I'm done", "Actually, one more thing...", "Never mind..."}));
+	}
+
+	IEnumerable displayTalkBoxMessages(string[] messages)
+	{
+		talkBox.SetActive(true);
+		for(int i = 0; i < messages.Length; i++)
+		{
+			talkText.text = messages[i];
+
+			while(!Input.GetMouseButtonDown(0))
+			{
+				yield return null;
+			}
+
+			yield return null;
+		}
+
+		talkBox.SetActive(false);
+	}
+}
---
+++ exercises/game03/Assets/Scripts/UnitScript.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitScript : MonoBehaviour
+{
+
+    public bool selected = false;
+	bool hover = false;
+
+	public string name;
+	public Sprite portrait;
+
+	Color defautColor;
+	public Color hoverColor;
+	public Color selectedColor; 
+
+	CharacterController cc;
+	public Vector3 destination;
+
+	public Renderer rend;
+
+	GameManager gm;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		//health = Random.Range(60,100);
+        GameObject gmObj = GameObject.Find("GameManagerObject");
+		gm = gmObj.GetComponent<GameManager>();
+
+        cc = gameObject.GetComponent<CharacterController>();
+
+        destination = transform.position;
+
+        defautColor = rend.material.color;
+
+		setColorOnMouseState();
+
+        transform.eulerAngles = new Vector3(0, Random.Range(0,360), 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (destination != null && Vector3.Distance(destination, transform.position) > 0.5f) 
+        {
+			destination.y = transform.position.y;
+			Vector3 vecToDest = (destination - transform.position).normalized;
+			float step = 3 * Time.deltaTime;
+			Vector3 newDir = Vector3.RotateTowards(transform.forward, vecToDest, step, 1);
+			transform.rotation = Quaternion.LookRotation(newDir);
+
+			cc.Move(transform.forward * 5 * Time.deltaTime);
+		}
+    }
+
+	public void setColorOnMouseState()
+	{
+		if (selected) 
+		{
+			rend.material.color = selectedColor;
+		} 
+		else if (hover) 
+		{
+			rend.material.color = hoverColor;
+		} 
+		else 
+		{
+			rend.material.color = defautColor;
+		}
+	}
+
+	private void OnMouseOver()
+	{
+		hover = true;
+		setColorOnMouseState();
+
+		/* gm.healthMeterObject.SetActive(true);
+		gm.meterFG.fillAmount = health / 100;
+		gm.healthMeterObject.transform.position = Camera.main.WorldToScreenPoint(
+																		transform.position + Vector3.up * 2); */
+	}
+	private void OnMouseExit()
+	{
+		gm.healthMeterObject.SetActive(false);
+
+		hover = false;
+		setColorOnMouseState();
+	}
+	private void OnMouseDown()
+	{
+		selected = !selected;
+		if (selected) 
+		{
+			gm.selectUnit(this);
+		} 
+		else 
+		{
+			gm.selectUnit(null);
+		}
+		setColorOnMouseState();
+	}
+}
